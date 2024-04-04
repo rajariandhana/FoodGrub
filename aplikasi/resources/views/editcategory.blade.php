f7efc3bf52e1fc1efde087d29d6b25fb210d4e14
rajariandhana <rajariandhana@gmail.com>
2024-04-04 15:00:41 +0700

    hhhm

--- aplikasi/resources/views/editcategory.blade.php
+++ aplikasi/resources/views/editcategory.blade.php
@@ -1,19 +1,21 @@
 @extends('layout')
+@section('css')
+    <link rel="stylesheet" href="{{ url('css/menulist.css') }}">
+@endsection
 
 @section('container')
     {{-- <h3>{{ $category->nama }}</h3> --}}
     <div class="update-category">
-        <form action="/update_category/{{$category->id}}" method="POST" enctype="multipart/form-data"
-            class="input-submit">
+        <form action="/update_category/{{ $category->id }}" method="POST" enctype="multipart/form-data" class="input-submit">
             @csrf
             @method('put')
             <input type="text" name="nama" placeholder="category name">
             <button type="submit" class="orange">Update Category</button>
             @error('nama')
-                <div class="alert alert-danger">{{$message}}</div>
+                <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </form>
-        
+
     </div>
     <script>
         function editFields(MenuId) {
@@ -24,15 +26,20 @@
             var put = 'put';
 
             // Replace the text with input fields
-            document.getElementById('tr' + MenuId).innerHTML = '<td><form action="/update_menu/'+ MenuId +'" method="POST" enctype="multipart/form-data">@csrf @method('+put+')<td><input type="text" name="nama" value="' + nama + '"></td><td><input type="text" name="harga" value="' + harga + '"></td><td><input type="text" name="desc" value="' + desc + '"><input type="text" class="form-control" value="'+ MenuId +'" name="category_id" style="display: none"></td><td><button type="submit" class="button-update">Save</button></td></form></td>';
-            
+            document.getElementById('tr' + MenuId).innerHTML = '<td><form action="/update_menu/' + MenuId +
+                '" method="POST" enctype="multipart/form-data">@csrf @method('+put+')<td><input type="text" name="nama" value="' +
+                nama + '"></td><td><input type="text" name="harga" value="' + harga +
+                '"></td><td><input type="text" name="desc" value="' + desc +
+                '"><input type="text" class="form-control" value="' + MenuId +
+                '" name="category_id" style="display: none"></td><td><button type="submit" class="button-update">Save</button></td></form></td>';
+
             //document.getElementById('nama' + MenuId).innerHTML = '<input type="text" name="nama" value="' + nama + '">';
             //document.getElementById('harga' + MenuId).innerHTML = '<input type="text" name="harga" value="' + harga + '">';
             //document.getElementById('desc' + MenuId).innerHTML = '<input type="text" name="desc" value="' + desc + '"><input type="text" class="form-control" value="'+ MenuId +'" name="category_id" style="display: none">';
             //document.getElementById('editButton' + MenuId).innerHTML = '<button type="submit" class="button-update">Save</button>';
         }
 
-        function edit(MenuId){
+        function edit(MenuId) {
             var nama = document.getElementById('nama' + MenuId).innerText;
             var harga = document.getElementById('harga' + MenuId).innerText;
             var desc = document.getElementById('desc' + MenuId).innerText;
@@ -42,113 +49,139 @@
             inputHarga.value = harga;
             var inputDesc = document.getElementById('editDesc');
             inputDesc.value = desc;
-            var inputCatId= document.getElementById('editCatId');
+            var inputCatId = document.getElementById('editCatId');
             inputCatId.value = MenuId;
             var formElement = document.getElementById('editForm');
-            formElement.action = '/update_menu/'+MenuId;
+            formElement.action = '/update_menu/' + MenuId;
         }
     </script>
     <table>
         <thead>
             <tr>
-                <th>Nama</th>
-                <th>Harga</th>
-                <th>Deskripsi</th>
-                <th></th>
+                <th class="menuNama">Nama</th>
+                <th class="menuHarga">Harga</th>
+                <th class="menuDesc">Deskripsi</th>
+                <th class=""></th>
+                <th class=""></th>
             </tr>
         </thead>
         <tbody>
-            @foreach($category->menus as $menu)
+            @foreach ($category->menus as $menu)
                 <tr id="tr{{ $menu->id }}">
-                    <td id="nama{{ $menu->id }}">{{ $menu->nama }}</td>
-                    <td id="harga{{ $menu->id }}">{{ $menu->harga }}</td>
-                    <td id="desc{{ $menu->id }}">{{ $menu->desc }}</td>
+                    <td class="menuNama" id="nama{{ $menu->id }}">{{ $menu->nama }}</td>
+                    <td class="menuHarga" id="harga{{ $menu->id }}">{{ $menu->harga }}</td>
+                    <td class="menuDesc" id="desc{{ $menu->id }}">{{ $menu->desc }}</td>
                     <td>
-                        <form id="deleteForm" action="/delete_menu/{{$menu->id}}" method="POST" enctype="multipart/form-data">
+                        <formv id="deleteForm" action="/delete_menu/{{ $menu->id }}" method="POST"
+                            enctype="multipart/form-data">
                             @csrf
                             @method('delete')
-                            <button type="submit" class="button-delete"
-                            onclick="return confirm('Are you sure you want to delete this menu?')">Delete Menu</button>
-                        </form>
+                            <button type="submit" class="red"
+                                onclick="return confirm('Are you sure you want to delete this menu?')">Delete Menu</button>
+                        </formv>
                     </td>
-                    <td id ="editButton{{ $menu->id }}"><button onclick="edit('{{ $menu->id }}')" class="button-delete">Edit</button></td>
+                    <td id ="editButton{{ $menu->id }}"><button onclick="edit('{{ $menu->id }}')"
+                            class="orange">Edit</button></td>
                 </tr>
             @endforeach
-            <form action="/create_menu" method="POST" enctype="multipart/form-data">
+            <tr>
+                <form action="/create_menu" method="POST" enctype="multipart/form-data">
                     @csrf
-                    <td>
-                        <input type="text" class="form-control" name="nama" placeholder="Enter Name">
+
+                    <td class="menuNama">
+                        <input class="input-create" type="text" class="form-control" name="nama"
+                            placeholder="Enter Name">
                         @error('nama')
                             <div class="alert alert-danger">
                                 {{ $message }}
                             </div>
                         @enderror
                     </td>
-                    <td>
-                        <input type="text" class="form-control" name="harga" placeholder="price">
+                    <td class="menuHarga">
+                        <input class="input-create" type="text" class="form-control" name="harga" placeholder="price">
                         @error('price')
                             <div class="alert alert-danger">
                                 {{ $message }}
                             </div>
                         @enderror
                     </td>
-                    <td>
-                        <input type="text" class="form-control" name="desc" placeholder="Enter description">
+                    <td class="menuDesc">
+                        <input class="input-create" type="text" class="form-control" name="desc"
+                            placeholder="Enter description">
                         @error('description')
                             <div class="alert alert-danger">
                                 {{ $message }}
                             </div>
                         @enderror
-                        <input id="editCatId" type="text" class="form-control" value="{{$category->id}}" name="category_id" style="display: none">
+                        <input id="editCatId" type="text" class="form-control" value="{{ $category->id }}"
+                            name="category_id" style="display: none">
                     </td>
-                    <td><button type="submit" class="button-create">Creat Edit Menu</button></td>
+                    <td><button type="submit" class="green">Create Menu</button></td>
+
                 </form>
-                <form id="editForm" action="/update_menu/" method="POST" enctype="multipart/form-data">@csrf @method('+put+')
+            </tr>
+            <tr>
+                <form id="editForm" action="/update_menu/" method="POST" enctype="multipart/form-data">@csrf
+                    @method('+put+')
                     @csrf
                     @method('put')
-                    <input id="editNama" type="text" name="nama">
-                    <input id="editHarga" type="text" name="harga">
-                    <input id="editDesc" type="text" name="desc"><input id="editCatId" type="text" class="form-control" name="category_id" style="display: none">
-                    <button type="submit" class="button-update">Save</button>
+
+                    <td class="menuNama">
+                        <input class="input-create" id="editNama" type="text" name="nama">
+                    </td>
+                    <td class="menuHarga">
+                        <input class="input-create" id="editHarga" type="text" name="harga">
+                    </td>
+                    <td class="menuDesc">
+                        <input class="input-create" id="editDesc" type="text" name="desc">
+                    </td>
+                    <td>
+                        <input id="editCatId" type="text" class="form-control" name="category_id" style="display: none">
+                    </td>
+                    <td>
+                        <button type="submit" class="green">Save</button>
+                    </td>
+
                 </form>
+            </tr>
             <!--<tr>
-            <form action="/create_menu" method="POST" enctype="multipart/form-data">
-                    @csrf
-                    <td>
-                        <input type="text" class="form-control" name="nama" placeholder="Enter Name">
-                        @error('nama')
-                            <div class="alert alert-danger">
-                                {{ $message }}
-                            </div>
-                        @enderror
-                    </td>
-                    <td>
-                        <input type="text" class="form-control" name="harga" placeholder="price">
-                        @error('price')
-                            <div class="alert alert-danger">
-                                {{ $message }}
-                            </div>
-                        @enderror
-                    </td>
-                    <td>
-                        <input type="text" class="form-control" name="desc" placeholder="Enter description">
-                        @error('description')
-                            <div class="alert alert-danger">
-                                {{ $message }}
-                            </div>
-                        @enderror
-                        <input type="text" class="form-control" value="{{$category->id}}" name="category_id" style="display: none">
-                        
-                    </td>
-                    <td><button type="submit" class="button-create">Create Menu</button></td>
-                </form>
-            </tr>-->
+                            <form action="/create_menu" method="POST" enctype="multipart/form-data">
+                                    @csrf
+                                    <td>
+                                        <input type="text" class="form-control" name="nama" placeholder="Enter Name">
+                                        @error('nama')
+        <div class="alert alert-danger">
+                                                                {{ $message }}
+                                                            </div>
+    @enderror
+                                    </td>
+                                    <td>
+                                        <input type="text" class="form-control" name="harga" placeholder="price">
+                                        @error('price')
+        <div class="alert alert-danger">
+                                                                {{ $message }}
+                                                            </div>
+    @enderror
+                                    </td>
+                                    <td>
+                                        <input type="text" class="form-control" name="desc" placeholder="Enter description">
+                                        @error('description')
+        <div class="alert alert-danger">
+                                                                {{ $message }}
+                                                            </div>
+    @enderror
+                                        <input type="text" class="form-control" value="{{ $category->id }}" name="category_id" style="display: none">
+                                        
+                                    </td>
+                                    <td><button type="submit" class="button-create">Create Menu</button></td>
+                                </form>
+                            </tr>-->
         </tbody>
     </table>
-    <form id="deleteForm" action="/delete_category/{{$category->id}}" method="POST" enctype="multipart/form-data">
+    <form id="deleteForm" action="/delete_category/{{ $category->id }}" method="POST" enctype="multipart/form-data">
         @csrf
         @method('delete')
         <button type="submit" class="red"
-        onclick="return confirm('Are you sure you want to delete this category?')">Delete Category</button>
+            onclick="return confirm('Are you sure you want to delete this category?')">Delete Category</button>
     </form>
 @endsection
