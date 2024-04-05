1cec34354f2e01ed2471611b0f09f4b7890b0db4
rajariandhana <rajariandhana@gmail.com>
2024-04-05 13:05:55 +0700

    dahlah

--- aplikasi/resources/views/layout.blade.php
+++ aplikasi/resources/views/layout.blade.php
@@ -30,8 +30,10 @@
         {{-- <li><a href="/categories" class="">categories</a></li> --}}
         {{-- <li><a href="/edit">edit</a></li> --}}
     </header>
-    <div>
+    <div class="content">
+        <div class="content2">
         @yield('container')
+    </div>
     </div>
     {{-- <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script> --}}
     {{-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> --}}
