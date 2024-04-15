2555b27e3d53ddbbc30bcf53e6f7f4a707a94e93
PutraAdam08 <putraadam322@gmail.com>
2024-04-15 16:27:17 +0700

    batre mau habis, create sama delete controller udah

--- aplikasi/app/Http/Controllers/DiscountController.php
+++ aplikasi/app/Http/Controllers/DiscountController.php
@@ -15,4 +15,36 @@ class DiscountController extends Controller
             // 'daftarmenu'=>Category::tipe()
         ]);
     }
+
+    public function create_discount(Request $request)
+    {
+        $request->validate([
+            'minimumBeli'=>'required',
+            'potonganHarga'=>'required',
+            'diskon_mulai'=>'required',
+            'diskon_selesai'=>'required',
+        ],
+        [
+            'minimumBeli.required'=>'Minimum spending cannot be empty',
+            'potonganHarga.required'=>'Discount cannot be empty',
+            'diskon_mulai.required'=>'Starting date cannot be empty',
+            'diskon_selesai.required'=>'Expired date cannot be empty',
+        ]);
+        Discount::create([
+            'minumumBeli'=>$request->minimumBeli,
+            'potonganHarga'=>$request->potonganHarga,
+            'diskon_mulai'=>$request->diskonMulai,
+            'diskon_selesai'=>$request->diskonSelesai,
+        ]);
+        return back();
+    }
+
+
+    public function delete_discount()
+    {
+        $discount = Discount::findorfail($id);
+        $discount->delete();
+
+        return back();
+    }
 }
