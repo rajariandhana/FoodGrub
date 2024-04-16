682e5dcf20caf27351bcaae25b087a6b312d9ce8
PutraAdam08 <putraadam322@gmail.com>
2024-04-16 19:14:49 +0700

    Add edit delete create for discount

--- aplikasi/app/Http/Controllers/DiscountController.php
+++ aplikasi/app/Http/Controllers/DiscountController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Discount;
+use Carbon\Carbon;
 
 class DiscountController extends Controller
 {
@@ -31,20 +32,47 @@ class DiscountController extends Controller
             'diskon_selesai.required'=>'Expired date cannot be empty',
         ]);
         Discount::create([
-            'minumumBeli'=>$request->minimumBeli,
+            'minimumBeli'=>$request->minimumBeli,
             'potonganHarga'=>$request->potonganHarga,
-            'diskon_mulai'=>$request->diskonMulai,
-            'diskon_selesai'=>$request->diskonSelesai,
+            'diskon_mulai' => Carbon::parse($request->input('diskon_mulai'))->format('Y-m-d'),
+            'diskon_selesai' => Carbon::parse($request->input('diskon_selesai'))->format('Y-m-d'),
         ]);
         return back();
     }
 
+    public function update_discount(Request $request, $id)
+    {
+        $request->validate([
+            'minimumBeli'=>'required',
+            'potonganHarga'=>'required',
+            'diskon_mulai'=>'required',
+            'diskon_selesai'=>'required',
+        ],
+        [
+            'minimumBeli.required'=>'Minimum spending cannot be empty',
+            'potonganHarga.required'=>'Discount cannot be empty',
+            'diskon_mulai.required'=>'Starting date cannot be empty',
+            'diskon_selesai.required'=>'Expired date cannot be empty',
+        ]);
+        $discount = Discount::findorfail($id);
+        $new_data = [
+            'minimumBeli'=>$request->minimumBeli,
+            'potonganHarga'=>$request->potonganHarga,
+            'diskon_mulai' => Carbon::parse($request->input('diskon_mulai'))->format('Y-m-d'),
+            'diskon_selesai' => Carbon::parse($request->input('diskon_selesai'))->format('Y-m-d'),
+        ];
+        $discount->update($new_data);
+        return back();
+    }
 
-    public function delete_discount()
+
+    public function delete_discount($id)
     {
         $discount = Discount::findorfail($id);
         $discount->delete();
 
         return back();
     }
+
+
 }
